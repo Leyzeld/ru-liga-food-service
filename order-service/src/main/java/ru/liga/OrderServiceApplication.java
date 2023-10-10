401f083b8b1f2b0fe571d3e8f0865cfae61689ed
Leyzeld <leyzeld@gmail.com>
2023-10-10 13:35:29 +0300

    Внес исправления

--- order-service/src/main/java/ru/liga/OrderServiceApplication.java
+++ order-service/src/main/java/ru/liga/OrderServiceApplication.java
@@ -2,9 +2,8 @@ package ru.liga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
